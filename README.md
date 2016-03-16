efb3fb9929a75133b4711bf3ad68adbb5cccfe75
Jami Gibbs <jami0821@gmail.com>
2016-03-16 09:56:35 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+### Wikipedia Viewer
+
+A small app that allows a user to search Wikipedia entries and also retrieves a random result.
+
+### Credit
+Data served by the [MediaWiki action API](https://www.mediawiki.org/wiki/API:Main_page)
---
+++ js/app.js
@@ -0,0 +1,105 @@
+/**
+  * We're using the module pattern here because reasons.
+  * @see https://carldanley.com/js-module-pattern/
+  */
+
+(function($, window, undefined) {
+
+  function WikiModule() {
+
+    /**
+     * Our function to call the Wikipedia API
+     * @param {string} generator - The way data is created (ie. 'random', 'allpages' ).
+     * @param {string} prop - Elements of the data returned (ie. 'images|extracts').
+     * @param {string} list - (ie. 'search').
+     * @param {string} srsearch - The search term (ie. 'cats').
+     */
+    function wikiGenerator(generator, prop, list, srsearch){
+      $.ajax({
+        url: 'http://en.wikipedia.org/w/api.php',
+        dataType: 'jsonp',
+        data: {
+          format: 'json',
+          action: 'query',
+          generator: generator,
+          grnnamespace: 0, // For the 'search' generator
+          prop: prop,
+          list: list,
+          srsearch: srsearch,
+          rvprop: 'content',
+          explaintext: '', // Plain text instead of limited HTML.
+          exintro: '' // Return only the content before the first section.
+          //grnlimit: 1
+        },
+        success: function(data){
+          wikiDisplay(data, srsearch);
+        },
+        error: function (errorMessage) {
+          console.error('Error: ', errorMessage);
+        }
+      });
+    }
+
+    /**
+     * Will display content returned from Wikipedia
+     * @param {Object} data - The data object returned.
+     * @param {string} searchItem - (optional) String to search for.
+     */
+    function wikiDisplay(data, searchItem){
+      console.log(data);
+      // Search results
+      if(data.query.search){
+        $('#results').prepend($('<h2>').text('Results for: ' + searchItem));
+
+        for(var i = 0; i < data.query.search.length; i++){
+          var title = data.query.search[i].title;
+          var snippet = data.query.search[i].snippet;
+          $("#results").append("<li><a target='_blank' href='http://en.wikipedia.org/wiki/" + title + "'>" + title + "</a><p>" + snippet +  "...</p></li>");
+        }
+      // Random article
+      } else {
+        var pages = data.query.pages;
+        var page = pages[Object.keys(pages)[0]];
+
+        $('#display').append("<h2><a target='_blank' href='http://en.wikipedia.org/wiki/" + page.title + "'>" + page.title + "</a></h2>");
+        $('#display').append($('<p>').text(page.extract));
+      }
+    }
+
+    /**
+     * Generate our random wiki article
+     */
+    this.wikiRandom = function wikiRandom(){
+      $('#random-btn').click(function () {
+        $('#display').empty();
+        $('#results').empty();
+
+        wikiGenerator('random', 'revisions|images|extracts');
+      });
+    };
+
+    /**
+     * Generate our wiki search results
+     */
+    this.wikiResults = function wikiResults(){
+      $('#get-results').click(function () {
+        var searchItem = document.getElementById('search-text').value;
+
+        $('#results').empty();
+        $('#search-text').val('');
+        $('#display').empty();
+
+        wikiGenerator('allpages', null, 'search', searchItem);
+      });
+    };
+
+  }
+
+  // Expose access to the constructor
+  window.WikiModule = WikiModule;
+
+})(window.jQuery, window);
+
+var WikiModule = new WikiModule();
+WikiModule.wikiRandom();
+WikiModule.wikiResults();
